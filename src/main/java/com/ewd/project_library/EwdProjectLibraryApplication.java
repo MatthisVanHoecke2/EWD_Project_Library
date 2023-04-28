d0f23adef2c68e8a38ca9dfdb19d51720582b8a2
MatthisVanHoecke <matthis.vanhoecke@student.hogent.be>
2023-04-28 14:22:48 +0200

    First commit

---
+++ src/main/java/com/ewd/project_library/EwdProjectLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ewd.project_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwdProjectLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwdProjectLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ewd/project_library/EwdProjectLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewd.project_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwdProjectLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
